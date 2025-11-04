bbd70a18ae6753adb9e9cec0d282a69f0fe7758a
Yaroslav Kryvyi <krivoy.znu@gmail.com>
2025-11-04 02:11:07 +0200

    Lab10: add ThemeContext and toggle; update scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
+import {useTheme} from "../context/ThemeContext.jsx";
+
 export default function Header() {
+    const {theme, toggleTheme} = useTheme();
     return (
         <header className="header">
             <div className="header__inner">
-                <h1 style={{margin: 0, color: 'var(--brand)'}}>Лабораторна робота № 9</h1>
+                <h1 style={{margin: 0, color: 'var(--brand)'}}>Лабораторна робота № 10</h1>
             </div>
             <nav className="nav" aria-label="Головна навігація">
                 <a className="nav__link" href="#">Головна</a>
                 <a className="nav__link" href="#">Про нас</a>
-                <a className="nav__link nav__link--cta" href="#">Контакти</a>
+                <a className="nav__link" href="#">Контакти</a>
+                <div className="flex ml-auto gap-0.5">
+                    <button
+                        type="button"
+                        className="nav__link nav__link--cta"
+                        onClick={toggleTheme}
+                        title="Перемикач теми"
+                    >
+                        {theme === 'light' ? 'Темна' : 'Світла'}
+                    </button>
+                </div>
             </nav>
         </header>
     )
