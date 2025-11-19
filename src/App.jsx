248253fee33de3b51f3c8b3b40a203900260a99b
Yaroslav Kryvyi <krivoy.znu@gmail.com>
2025-11-19 11:29:33 +0200

    Lab12: [Add] page NotFound.jsx; splat-route in Aoo.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RootLayout from "./layouts/RootLayout.jsx";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Contacts from "./pages/Contacts.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contacts" element={<Contacts />} />
+                <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
     )
