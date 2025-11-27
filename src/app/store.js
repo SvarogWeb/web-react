a4d0d82435667f0769cb9dd57d9f2a9a540646c4
Yaroslav Kryvyi <krivoy.znu@gmail.com>
2025-11-27 15:30:36 +0200

    Lab13: [Create] employeesSlice and employeesThunks; [Edit] store.js and About.jsx

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import themeReducer from '../features/theme/themeSlice';
+import employeesReducer from '../features/employees/employeesSlice';
 
 export const store = configureStore({
     reducer: {
-        theme: themeReducer
+        theme: themeReducer,
+        employees: employeesReducer,
     },
     devTools: {
         name: 'Web React Lab 13',
